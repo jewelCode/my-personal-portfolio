c4e9bb670cc29eee581b977cc7a14f2a52067542
Jewel Barua <jewelbarua37@gmail.com>
2021-11-17 20:45:11 +0600

    services added

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -2,17 +2,23 @@ import React from 'react';
 
 const Services = () => {
     return (
-        <div>
-            <h2>My Services</h2>
-            <div className="row">
-                    <div className="col-md-4">
+        <div className="container mt-5">
+            <h2 className="text-center">My Services</h2>
+            <div className="row mt-5 justify-content-center">
+                    <div className="col-md-3 border p-5 me-5">
                         <h5 className="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
-                    <div className="col-md-4">
-
-                    </div>
-                    <div className="col-md-4">
-
+                    <div className="col-md-3 border p-5 me-5">
+                        <h5 className="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    <div className="col-md-3 border p-5 me-5">
+                        <h5 className="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
             </div>
             
