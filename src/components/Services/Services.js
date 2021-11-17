6bfd34521ce2b1d8fee0c1cb52be177c03721e0e
Jewel Barua <jewelbarua37@gmail.com>
2021-11-17 21:10:29 +0600

    service added

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -8,17 +8,17 @@ const Services = () => {
                     <div className="col-md-3 border p-5 me-5">
                         <h5 className="card-title">Card title</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        
                     </div>
                     <div className="col-md-3 border p-5 me-5">
                         <h5 className="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            
                         </div>
                     <div className="col-md-3 border p-5 me-5">
                         <h5 className="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        
                     </div>
             </div>
             
